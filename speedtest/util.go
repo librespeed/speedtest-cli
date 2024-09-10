d33e431b58774807f608f7a38bd5e5fd9cc3dfc4
Maddie Zhan <maddie.zhan@cynovan.com>
2024-09-10 23:22:34 +0800

    fix build for non-windows/linux platforms

--- speedtest/util.go
+++ speedtest/util.go
@@ -1,3 +1,6 @@
+//go:build !linux
+// +build !linux
+
 package speedtest
 
 import (
@@ -6,5 +9,5 @@ import (
 )
 
 func newDialerInterfaceBound(iface string) (dialer *net.Dialer, err error) {
-	return nil, fmt.Errorf("cannot bound to interface on Windows")
+	return nil, fmt.Errorf("cannot bound to interface on this platform")
 }
